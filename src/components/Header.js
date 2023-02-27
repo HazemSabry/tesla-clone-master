4c1901ad73e3a4ea9c27c8917ea04f482eef38db
HazemSabry <89419013+HazemSabry@users.noreply.github.com>
2023-02-27 23:54:06 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,20 +13,20 @@ function Header() {
   console.log(cars);
   return (
     <Container>
-      <a href="">
+      <a href="#">
         <img src="/images/logo.svg" alt="logo" />
       </a>
       <Menu>
         {cars &&
           cars.map((car) => (
-            <a key={car} href="">
+            <a key={car} href="#">
               {car}
             </a>
           ))}
       </Menu>
       <RightMenu>
-        <a href="">Shop</a>
-        <a href="">Tesla Account</a>
+        <a href="#">Shop</a>
+        <a href="#">Tesla Account</a>
         <CustomMenu onClick={() => setBurgerStatus(true)} />
       </RightMenu>
       <BurgerNav show={burgerStatus}>
@@ -37,7 +37,7 @@ function Header() {
           {cars &&
             cars.map((car) => (
               <li>
-                <a key={car} href="">
+                <a key={car} href="#">
                   {car}
                 </a>
               </li>
